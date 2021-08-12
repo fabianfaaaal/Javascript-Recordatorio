4e92a3ddcf3b9fb4b175a8fc7801e43bdcf75823
fabianfaaal <fabian.lammoglia@gmail.com>
2021-08-12 00:17:48 -0300

    Listo Variables, sigue Scope

--- repasojs/js/app.js
+++ repasojs/js/app.js
@@ -12,3 +12,22 @@ console.log(aprendiendo); //Vemos el contenido de nuestras variables.
 
 console.log(aprendiendo99);
 
+
+//Variables con var
+var aprendiendo1 = "JavaScript";
+
+console.log(aprendiendo1);
+
+const aprendiendo2 = "JavaScript constante"; //Debes siempre inicializar una variable const con un valor para no tener errores.
+// aprendiendo2 = true; no puedes cambiar el valor de una variable constante, por eso se llaman constantes.
+
+console.log(aprendiendo2);
+
+//Variables con let
+
+let aprendiendo3; // let se puede inicilizar vacio.
+
+aprendiendo3 = true;
+
+console.log(aprendiendo3);
+
