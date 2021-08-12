2ad3ad8da9fa4dbd7c46b94c21af52dea1018e74
fabianfaaal <fabian.lammoglia@gmail.com>
2021-08-11 23:56:46 -0300

    first commit

---
+++ repasojs/js/app.js
@@ -0,0 +1,13 @@
+//Crear variables
+
+var aprendiendo99 = true; //Variable booleana
+
+aprendiendo99 = false; //Cambiamos el contenido de esa variable
+aprendiendo99 = 20; //JS admite cambiar entre tipos de variables en una misma variable
+aprendiendo99 ="Juan";
+
+var aprendiendo; //Podemos iniciar variables sin obligacion de iniciarla con datos.
+
+console.log(aprendiendo); //Vemos el contenido de nuestras variables.
+
+console.log(aprendiendo99);
