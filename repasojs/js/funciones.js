ada5a47f20568d8b3c2813c5e106bb6ed8703801
fabianfaaal <fabian.lammoglia@gmail.com>
2021-08-16 23:56:45 -0300

    Funciones

---
+++ repasojs/js/funciones.js
@@ -0,0 +1,12 @@
+//Creando una función
+
+console.log("Bienvenido");
+
+//Function declaration
+function saludar(nombre){
+    console.log("Bienvenido" + nombre);
+}
+
+saludar("Juan");
+saludar("Alejandra");
+saludar("Miguel");
