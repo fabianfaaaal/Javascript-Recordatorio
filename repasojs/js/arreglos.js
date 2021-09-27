a3fbd64156d71f0e6e27df90d929b574295e1085
fabianfaaal <fabian.lammoglia@gmail.com>
2021-09-27 17:57:02 -0300

    Add/ arreglos file

--- repasojs/js/arreglos.js
+++ repasojs/js/arreglos.js
@@ -4,11 +4,26 @@ const carrito = ["producto1", "producto2", "producto3"];
 
 console.log(carrito);
 
-const appContenedor = document.querySelector("#app");
+// const appContenedor = document.querySelector("#app");
 
-let html = "";
-carrito.forEach((producto) => {
-  html += `<li>${producto}</li>`;
-});
+// let html = "";
+// carrito.forEach((producto) => {
+//   html += `<li>${producto}</li>`;
+// });
 
-appContenedor.innerHTML = html;
+// appContenedor.innerHTML = html;
+
+carrito.map(producto => {
+  return 'El producto es '+ producto
+})
+
+//Object.keys
+
+const persona = { 
+  nombre: 'Juan',
+  profesion: 'Desarrollador web',
+  edad: 500
+}
+
+console.log(Object.keys(persona))
+
